e806851cbb16fe51b1964ab273f25224ab94dd84
Vladislav <nicochan00@mail.ru>
2022-07-07 17:33:25 +0300

    finalSimple slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const sliderLine = document.querySelector('.slider-line');
+let offset = 0;
+
+document.querySelector('.slider-next').addEventListener('click', () => {
+   offset += 256;
+   if (offset > 768) {
+      offset = 0;
+   }
+   sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', () => {
+   offset -= 256;
+   if (offset < 0) {
+      offset = 768;
+   }
+   sliderLine.style.left = -offset + 'px';
+});
